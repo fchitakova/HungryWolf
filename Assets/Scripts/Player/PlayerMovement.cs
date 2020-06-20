e75e4c81756ca44b499c45b9814aca6cae2e97be
Fatmagyul <fatmagyul.chitakova@gmail.com>
2020-06-20 17:08:15 +0300

    add keep wolf in screen bounds feature

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed = 2.5f;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private Vector2 movement;
+    private Vector2 horizontalScreenBoundaries;
+    private Vector2 verticalScreenBoundaries;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        calculateScreenBoundaries();
+    }
+
+    void calculateScreenBoundaries()
+    {
+        float cameraZposition = Camera.main.transform.position.z;
+        Vector2 screenBoundaries = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cameraZposition));
+        float playerWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        float playerHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        horizontalScreenBoundaries = new Vector2((-screenBoundaries.x) + playerWidth, screenBoundaries.x - playerWidth);
+        verticalScreenBoundaries = new Vector2((-screenBoundaries.y)+ playerHeight, screenBoundaries.y - playerHeight);
+    }
+
+    void Update()
+    { 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 currentPosition = rigidBody.position;
+        Vector2 newPosition = currentPosition + (movement* moveSpeed *Time.fixedDeltaTime);
+
+        Vector2 newPositionInScreenBoundaries = clampNewPositionInScreenBoundaries(newPosition);
+
+        rigidBody.MovePosition(newPositionInScreenBoundaries);
+
+    }
+
+    Vector2 clampNewPositionInScreenBoundaries(Vector2 newPosition)
+    {
+        Vector2 newPositionInScreenBoundaries;
+        newPositionInScreenBoundaries.x = Mathf.Clamp(newPosition.x, horizontalScreenBoundaries.x, horizontalScreenBoundaries.y);
+        newPositionInScreenBoundaries.y = Mathf.Clamp(newPosition.y, verticalScreenBoundaries.x, verticalScreenBoundaries.y);
+        return newPositionInScreenBoundaries;
+    }
+
+}
---
+++ Assets/Scripts/SheepAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour {
+    private float moveSpeed = 0.5f;
+    private Vector2 targetSpot;
+    private float maxWaitTime;
+    public float remainingWaitTime;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    void Start () {
+        remainingWaitTime = maxWaitTime;
+    }
+
+    void Update () {
+        if (remainingWaitTime <= 0) {
+            targetSpot = new Vector2 (Random.Range (minX, maxX), Random.Range (minY, maxY));
+            StartCoroutine (moveTowardsTarget ());
+        }
+        remainingWaitTime -= Random.Range (1, 5);
+    }
+
+    IEnumerator moveTowardsTarget () {
+        while (Vector2.Distance (transform.position, targetSpot) <= 0.2f) {
+            Vector2 currentPosition = transform.position;
+            transform.position = Vector2.MoveTowards (currentPosition, targetSpot, moveSpeed * Time.deltaTime);
+            currentPosition = transform.position;
+
+            yield return null;
+        }
+        yield break;
+    }
+
+}
