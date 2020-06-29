a359ae7b6dd7e79d8c32e5bde02e83ee446b141e
Fatmagyul <fatmagyul.chitakova@gmail.com>
2020-06-29 23:07:36 +0300

    player health system on progress

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class PlayerHealth
+{
+    private const int maxHealth = 100;
+
+    private int currentHealth;
+
+    public PlayerHealth()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if(currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+    
+}
